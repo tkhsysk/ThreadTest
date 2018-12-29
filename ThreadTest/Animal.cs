c9e6c7c45b7984acdfc43f88c8758d0af713dc4b
DESKTOP-D4HG0LG\takta <takhs62syou@yahoo.co.jp>
2018-12-29 12:52:02 +0900

    単体テスト enum

---
+++ ThreadTest/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadTest
+{
+    [Flags]
+    public enum Animal
+    {
+        Dog = 0x01,
+        Cat = 0x02,
+        Bird = 0x100,
+    }
+
+    public class AnimalTest
+    {
+        public string GetAnimalString()
+        {
+            return Enum.GetName(typeof(Animal), Animal.Dog);
+        }
+
+        public void PrintAnimals()
+        {
+            foreach(Animal animal in Enum.GetValues(typeof(Animal)))
+            {
+                Debug.WriteLine(animal);
+            }
+        }
+
+        public Animal GetAnimal()
+        {
+            // ToObject: int -> enum
+            return (Animal)Enum.ToObject(typeof(Animal), 1);
+        }
+
+        public Animal ConvertStirngToAnimal()
+        {
+            return (Animal)Enum.Parse(typeof(Animal), "Dog");
+        }
+
+        // 拡張メソッド
+    }
+}
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ThreadTest;
 
@@ -7,6 +7,17 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        #region データ駆動を実行する際に必要
+
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        #endregion
+
         // Initialize
         // Cleanup
         // テストメソッドを修正したら、ビルドする必要ある
@@ -17,11 +28,40 @@ namespace UnitTestProject1
         // コードカバレッジの目安80%くらい
 
         [TestMethod]
+        [TestCategory("正常系テスト")]
         public void TestMethod1()
         {
             Stationery stationery = new Stationery();
 
             Assert.AreEqual(1, stationery.GetCounts());
+        }
+
+        [TestMethod]
+        [TestCategory("異常系テスト")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", @"test.csv", "test#csv", DataAccessMethod.Sequential)]
+        public void TestMethod2()
+        {
+            // https://qiita.com/mima_ita/items/55394bcc851eb8b6dc24
+            // test.csvは、sjisで保存すること
+
+            // https://docs.microsoft.com/ja-jp/visualstudio/test/how-to-create-a-data-driven-unit-test?view=vs-2015
+            // データ ドリブン テストのデータ ソース情報を格納する TestContext オブジェクトを作成します。 
+            //次に、フレームワークは作成する TestContext プロパティの値としてこのオブジェクトを設定します。
+
+            int a = (int)TestContext.DataRow["a"];
+            int b = (int)TestContext.DataRow["b"];
+            int result = (int)TestContext.DataRow["result"];
+
+            Debug.WriteLine(a + " " + b + " " + result);
+            Assert.AreEqual(result, a + b);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Debug.WriteLine((int)Animal.Dog);
+            Debug.WriteLine((int)Animal.Cat);
+            Debug.WriteLine((int)Animal.Bird);
         }
     }
 }
